be96881cf7f8aa79b63f9feaf349b700f17cd5fa
Esteban Puyo <jpuyoc@ucentral.edu.co>
2024-11-25 22:49:04 -0800

    dynamic content utilizing dictionary

--- challenges/views.py
+++ challenges/views.py
@@ -2,15 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
 
+monthly_challenges = {
+    "january": "Eat more meat",
+    "february": "Walk everyday",
+    "march": "Eat more chicken",
+    "april": "Learn Django"
+}
+
 def monthly_challenge_by_number(request, month):
+    
     return HttpResponse(month)
 
 def monthly_challenge(request, month):
-    challenge_text = None
-    if month == "january":
-        challenge_text = "This workds!"
-    elif month == "february":
-        challenge_text = "Walk More"
-    else:
-        HttpResponseNotFound("This month is not supported")
+    try:
+        challenge_text = monthly_challenges[month]
+    except:
+        return HttpResponseNotFound("Wrong Month")
     return HttpResponse(challenge_text)
