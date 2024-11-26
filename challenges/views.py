48ae45b0d82161d4b1c0862df54bbc3e1ffc484e
Esteban Puyo <jpuyoc@ucentral.edu.co>
2024-11-25 22:36:10 -0800

    add dynamic url for the months

--- challenges/views.py
+++ challenges/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
 
 
-def january(request):
-    return HttpResponse("This workds!")
-
-def february(request):
-    return HttpResponse("Walk More")
+def monthly_challenge(request, month):
+    challenge_text = None
+    if month == "january":
+        challenge_text = "This workds!"
+    elif month == "february":
+        challenge_text = "Walk More"
+    else:
+        HttpResponseNotFound("This month is not supported")
+    return HttpResponse(challenge_text)
