fb2be4ed6275d981fc8c308bad2ffcdb197b1528
Esteban Puyo <jpuyoc@ucentral.edu.co>
2024-11-25 22:58:55 -0800

    redirection for numbers

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 # Create your views here.
 
 monthly_challenges = {
@@ -10,8 +10,13 @@ monthly_challenges = {
 }
 
 def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
+
+    if month > len(months):
+        return HttpResponseNotFound("Wrong Month")
     
-    return HttpResponse(month)
+    redirect = months[month]
+    return HttpResponseRedirect("/challenges/" + redirect)
 
 def monthly_challenge(request, month):
     try:
